91b3616e5b869b97a9ae00d3f1a60d5158ee1461
RileyJJ794 <hoffman@Hoffmans-MacBook-Pro.local>
2019-11-14 14:17:17 -0600

    added js more

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
+numberPlays = 0;
+playerSym = '';
 
-let PlayerTurn = 
+function whosTurn(numberPlays, playerSym) {
+    if (numberPlays % 2 == 0) {
+        let playerSym = 'X';
+    } else {
+        let playerSym = "O"
+    };
+
+    return playerSym;
+};
 
 
-let cell = document.querySelectorAll('.cell')
+
+let cell = document.querySelectorAll('.cell');
 
 for (var i = 0; i < cell.length; i++) {
-    cell[i].addEventListener('click', function(){
+    cell[i].addEventListener('click', function () {
         let turn = whosTurn(this.innerHTML);
+        console.log(turn)
         // add something that shows an x or an o dependng on whos turn it is at the cell selected by player
 
     });
